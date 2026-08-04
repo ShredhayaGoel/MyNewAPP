8d3e1de363f9ef2d006129326b767f7b49f4c76e
Shredhaya <shredhayagoel@gmail.com>
2026-08-05 04:20:43 +0530

    splashscreen

---
+++ screens/splashscreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+export default function SplashScreen({ navigation }) {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.replace('Login');
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/logo.png')}
+                style={styles.logo}
+                resizeMode="contain"
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    logo: {
+        width: 280,
+        height: 120,
+    },
+});
