9a75b71baedfaee52a4780e01b85a5dbce71249e
Shredhaya <shredhayagoel@gmail.com>
2026-08-05 04:19:31 +0530

    appnavigator

--- App.jsx
+++ App.jsx
@@ -1,37 +1,6 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
 import React from 'react';
-import { Button } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-      <Image
-        style={{ width: 200, height: 200 }}
-        source={{
-          uri: 'https://cdna.artstation.com/p/marketplace/presentation_assets/003/430/262/large/file.jpg?1706950301',
-        }}
-      />
-
-      <Button title="Click Me"></Button>
-
-      <TouchableOpacity
-        style={{ padding: 10, backgroundColor: 'green' }}
-        onPress={() => Alert.alert('pressed')}
-      >
-        <Text>press me</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-export default App;
-
-const styles = StyleSheet.create({});
+export default function App() {
+  return <AppNavigator />;
+}
